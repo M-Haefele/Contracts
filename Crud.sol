8493e8e247e102c91845ca8163cfc29c3a2d05fc
Matt Haefele <mhaefele@uno.edu>
2019-07-06 22:31:16 -0500

    Structures and Crud contract:

---
+++ Crud.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.5.1;
+
+contract Crud{
+    struct User{
+        uint id;
+        string name;
+    }
+    User[] public users;
+    uint public nextId;
+    
+    function create(string memory name)public {
+        users.push(User(nextId, name));
+        nextId++;
+    }
+    
+    function read(uint id) view public returns(uint, string memory ){
+        for(uint i = 0; i < users.length; i++){
+            if(users[i].id == id){
+                return(users[i].id, users[i].name);
+            }
+        }
+    }
+    
+    function update(uint id, string memory name) public{
+        for(uint i = 0; i < users.length; i++){
+            if(users[i].id == id){
+               users[i].name = name;
+            }
+        }
+    }
+    
+    function remove(uint id) public {
+        delete users[id];
+    }
+    
+}
