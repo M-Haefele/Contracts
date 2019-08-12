26931e17bd38e1c27f499d5e39e3ba6290be52e4
Matt Haefele <mhaefele@uno.edu>
2019-08-12 13:08:49 -0500

    comments

--- Faucet.sol
+++ Faucet.sol
@@ -4,8 +4,10 @@ pragma solidity ^0.5.1;
 
 contract Faucet{
 	
-	function withdraw(uint withdraw_amount) public{
+	//basic faucet functionality
 
+	function withdraw(uint withdraw_amount) public{
+		//parameter to only allow within window of Ether denominations
 		require(withdraw_amount <= 100000000000000);
 
 		msg.sender.transfer(withdraw_amount);
